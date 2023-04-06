e7f5f0d2504ab64dd81f2b797530997078238cce
matias Alexis mazzanti <106558165+MatiMazzanti@users.noreply.github.com>
2023-04-06 10:21:43 -0300

    galeria

---
+++ fotos.js
@@ -0,0 +1,9 @@
+const fulimgbox = document.getElementById("fulimgbox");
+fulimg = document.getElementById("fulimg");
+function openImg(reference){
+    fulimgbox.style.display = "flex";
+    fulimg.src = reference;
+}
+function closeImg(){
+    fulimgbox.style.display = "none";
+}
