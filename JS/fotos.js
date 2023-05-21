4ba64725dca91b8c834eac827912563b286361d6
roemanuel <roemanuel2020@gmail.com>
2023-05-21 16:17:21 -0300

    Actualización general

--- JS/fotos.js
+++
@@ -1,9 +0,0 @@
-const fulimgbox = document.getElementById("fulimgbox");
-fulimg = document.getElementById("fulimg");
-function openImg(reference) {
-    fulimgbox.style.display = "flex";
-    fulimg.src = reference;
-}
-function closeImg() {
-    fulimgbox.style.display = "none";
-}
--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// MENÚ
+
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
@@ -6,3 +8,14 @@ ScrollReveal().reveal('.new',{delay: 500});
 ScrollReveal().reveal('.cards',{delay: 500});
 
 
+// FOTOS
+
+const fulimgbox = document.getElementById("fulimgbox");
+fulimg = document.getElementById("fulimg");
+function openImg(reference) {
+    fulimgbox.style.display = "flex";
+    fulimg.src = reference;
+}
+function closeImg() {
+    fulimgbox.style.display = "none";
+}
