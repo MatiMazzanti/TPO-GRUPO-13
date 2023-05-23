1315c1ab3e13608b5c9cae2c49a079eb06e4d13c
Gabriel Solano <108505259+GabetoDev@users.noreply.github.com>
2023-05-23 10:15:40 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,61 @@ function openImg(reference) {
 function closeImg() {
     fulimgbox.style.display = "none";
 }
+
+
+// CONTACTANOS - Formulario
+const formulario = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+
+const expresiones= {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{6,30}$/, /* minimo 6 letras a 40 */
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/ , //correo
+    telefono:/^\d{7,14}$/ //de 7 a 14 numeros
+}
+
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            validarCampoNombre(expresiones.nombre, e.target, 'nombre');
+            
+        break;
+        case "email":
+            validarCampoEmail(expresiones.email, e.target, 'email');
+        break;
+        case "telefono":
+            validarCampoTelefono(expresiones.telefono, e.target, 'telefono');
+        break;
+    }
+}
+
+const validarCampoNombre = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        console.log("funciona");
+        nombre.style.borderColor = "#FFFFFF";
+    } else {
+        nombre.style.borderColor = "#FF0F0F";
+    }      
+}
+const validarCampoEmail = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        email.style.borderColor = "#FFFFFF";
+    } else {
+        email.style.borderColor = "#FF0F0F";
+    }      
+}
+const validarCampoTelefono = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        telefono.style.borderColor = "#FFFFFF";
+    } else {
+        telefono.style.borderColor = "#FF0F0F";
+    }      
+}
+
+
+//cuando se levanta la tecla o se da click esta funcion valida el formulario
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario );
+    input.addEventListener('blur', validarFormulario );
+});
+
